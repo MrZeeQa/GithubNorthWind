52649284da9e8769cdfdce04519248e517d66c5a
Nicolas Bouquiaux <nicolas.bouquiaux@jomasecundair.be>
2020-03-13 17:42:12 +0100

    Connectionstring aangemaakt database, EmployeeOverview Pagina toegevoegd

---
+++ GithubNorthWind/App_Code/EmployeeCS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+using Northwind.Components;
+
+/// <summary>
+/// Summary description for EmployeeCS
+/// </summary>
+public class EmployeeCS
+{
+   public SqlDataReader GetEmployees()
+    {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandType = CommandType.StoredProcedure;
+        command.CommandText = "Get_Employee";
+        connection.Open();
+        SqlDataReader Reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+        return Reader;
+    }
+}
---
+++ GithubNorthWind/App_Code/NorthWindCS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+using Northwind.Components;
+
+/// <summary>
+/// Summary description for NorthWindCS
+/// </summary>
+
+    namespace Northwind.Components {
+
+    public class NorthWindCS
+    {
+
+ 
+    }
+}
